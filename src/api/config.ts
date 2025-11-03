9a5f03e518789dca19846cf707ad2e0b488ad8be
oriNuguri25 <byeongjun25@naver.com>
2025-11-03 16:51:27 +0900

    feat: vercel reverse proxy 적용 -> https to http 사용 위함

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,7 @@
 export const SERVER_URI = import.meta.env.VITE_SERVER_URI;
 
+// Vercel 프록시 및 Vite 프록시를 타기 위해 상대 경로로 고정
 export const API_URLS = {
-  RECRUITMENT: `${SERVER_URI}/backend/recruitment`,
-  MANAGER_REGISTER: `${SERVER_URI}/backend/auth/manager/register`,
+  RECRUITMENT: "/backend/recruitment",
+  MANAGER_REGISTER: "/backend/auth/manager/register",
 } as const;
